cc0ad6c1844d7605b2cd9642802f3dd206ac2323
Michael Cao <michael@databento.com>
2023-08-11 15:38:10 -0400

    ADD: Create landing page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+
+let colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    // './src/**/*.html',
+    // './src/**/*.js',
+    "./*.html",
+  ],
+  theme: {
+    colors: colors,
+  },
+  variants: {},
+  plugins: [],
+};
